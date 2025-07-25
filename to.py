58fe3b77bd811f33fce7368a8ea82c492fe06276
ChennojuTejaswi <chennojutejaswi@gmail.com>
2025-07-25 20:08:53 +0530

    Add files via upload

---
+++ to.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from docx import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import hashlib
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_text_from_docx(docx_files):
+    text = ""
+    for docx in docx_files:
+        doc = Document(docx)
+        for para in doc.paragraphs:
+            text += para.text + "\n"
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    try:
+        new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+        docs = new_db.similarity_search(user_question)
+    except TimeoutError:
+        st.error("Error: Timed out while embedding your question. Please try again.")
+        return
+
+    chain = get_conversational_chain()
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    hashed_password = hashlib.sha256("lucky684".encode('utf-8')).hexdigest()
+
+    st.set_page_config(page_title="Chat with Documents", layout="wide")
+
+    # Login logic
+    if "username" not in st.session_state:
+        st.title("Document Query System💁")
+        username = st.text_input("Username:")
+        password = st.text_input("Password:", type="password")
+        login_clicked = st.button("Login")
+
+        if login_clicked:
+            if username == "srihitha" and hashlib.sha256(password.encode('utf-8')).hexdigest() == hashed_password:
+                st.session_state["username"] = username
+            else:
+                st.error("Invalid username or password")
+
+    else:
+        st.header("Document Query System💁")
+        user_question = st.text_input("Ask a Question from the Word Documents")
+        if user_question:
+            user_input(user_question)
+        with st.sidebar:
+            st.title("Menu:")
+            docx_files = st.file_uploader("Upload your Word Documents and Click on the Submit & Process Button", accept_multiple_files=True, type=["docx"])
+            if st.button("Submit & Process"):
+                with st.spinner("Processing..."):
+                    raw_text = get_text_from_docx(docx_files)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+        # Logout button
+        logout_button = st.button("Logout")
+        if logout_button:
+            del st.session_state["username"]
+            st.experimental_rerun()  # Clear session state and rerun the app
+
+if __name__ == "__main__":
+    main()
